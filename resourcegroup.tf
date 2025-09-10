7ddc7f8d3f2adf82df191ad3070b9c3bf32c98ad
kamalkaur1 <gurs7401@gmail.com>
2025-09-10 19:19:56 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "terraformmay022025" {
-  name     = "mcit_resource_group_may022025"
+  name     = "mcit_resource_group_kaur2025"
   location = "canadacentral"
 }
 resource "azurerm_resource_group" "terraformseptember" {
-  name     = "mcit_resource_group_sep092025"
+  name     = "mcit_resource_group_kaur2026"
   location = "canadacentral"
 }
