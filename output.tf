70799fa9d34ab8b9aa7fe1a41dbad13bb0abfa62
kamalkaur1 <gurs7401@gmail.com>
2025-09-12 19:23:20 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,3 +4,23 @@ value=var.firstname
 output "lastoutput"{
   value=var.lastname
 }
+output  "restraunttoutput1"{
+value=var.restraunt1
+}
+output  "restraunttoutput2"{
+value=var.restraunt2
+}
+output  "restraunttoutput3"{
+value=var.restraunt3
+}
+output  "restraunttoutput4"{
+value=var.restraunt4
+}
+output  "restraunttoutput5"{
+value=var.restraunt5
+}
+
+
+
+
+
