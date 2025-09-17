710a5e8b7544efc91718711a9c0bbf0aee867e1c
kamalkaur1 <gurs7401@gmail.com>
2025-09-17 18:56:37 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -30,9 +30,9 @@ locals {
    for s in var.raw_scores : {
      name  = split(":", s)[0]
      score = tonumber(split(":", s)[1])
-   }
- ]
- scores_map   = { for p in local.score_pairs : p.name => p.score }
+   ] 
+    }
+  scores_map   = { for p in local.score_pairs : p.name => p.score }
  average      = length(local.score_pairs) == 0 ? 0 :
                 sum([for p in local.score_pairs : p.score]) / length(local.score_pairs)
 }
