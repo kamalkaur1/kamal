2142def55987ecc7fa259ffdc7833f130c0f61aa
kamalkaur1 <gurs7401@gmail.com>
2025-09-17 19:08:34 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -125,7 +125,7 @@ variable "raw_scores" {
  type    = list(string)
  default = ["alice:90", "bob:75", "carol:85"]
 }
-variable "items" {
+variable "items1" {
  type    = list(string)
  default = ["b", "a", "c", "a", "b"]
 }
