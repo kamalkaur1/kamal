ff3b495ba15ab4b6c2c2f40238e0d25c6919f7db
kamalkaur1 <gurs7401@gmail.com>
2025-09-17 18:47:39 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -32,6 +32,7 @@ locals {
      score = tonumber(split(":", s)[1])
  ]
  } 
+ }
  scores_map   = { for p in local.score_pairs : p.name => p.score }
  average      = length(local.score_pairs) == 0 ? 0 :
                 sum([for p in local.score_pairs : p.score]) / length(local.score_pairs)
