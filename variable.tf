90865a692f891fa9c04b081b0a3ced780d6ec5cd
kamalkaur1 <gurs7401@gmail.com>
2025-09-17 20:12:27 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -121,10 +121,6 @@ variable "domain" {
  type    = string
  default = "example.com"
 }
-variable "raw_scores" {
- type    = list(string)
- default = ["alice:90", "bob:75", "carol:85"]
-}
 variable "items1" {
  type    = list(string)
  default = ["b", "a", "c", "a", "b"]
@@ -141,6 +137,7 @@ variable "words" {
  type    = list(string)
  default = ["hi", "cat", "car", "tree", "hi", "car"]
 }
+
 variable "raw_scores" {
  type    = list(string)
  default = ["alice:90", "bob:75", "carol:85"]
