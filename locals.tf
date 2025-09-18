1f82c2dffe85c6157eb389bc3d453b880ada74c3
kamalkaur1 <gurs7401@gmail.com>
2025-09-17 20:20:40 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -48,6 +48,6 @@ locals {
    }
  ]
 scores_map = { for p in local.score_pairs : p.name => p.score }
- average = length(local.score_pairs) == 0 ? 0 : sum([for p in local.score_pairs : p.score]) / length(local.score_pairs)
+ average = length(local.score_pairs) == 0 ? 0 :sum([for p in local.score_pairs : p.score]) / length(local.score_pairs)
 }
 
