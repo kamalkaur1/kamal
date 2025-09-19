921348fbbf21e8341cfb3aba4a13e37a0a85a559
kamalkaur1 <gurs7401@gmail.com>
2025-09-19 18:16:17 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -86,5 +86,11 @@ output "squares" { value = local.squares }
 output "grouped_by_initial" { value = local.grouped }
 output "scores_map" { value = local.scores_map }
 output "avg_score"  { value = local.average    }
+output  "hotels"{
+value=var.hotels
+}
+output "hotels" {
+  value = replace(var.hotels, "hyatt", upper("hyatt"))
+}
 
 
