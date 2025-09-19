6d50b68e31b3942dbf59fac040fa9ffe90d5f062
kamalkaur1 <gurs7401@gmail.com>
2025-09-19 19:06:44 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -87,3 +87,5 @@ output "grouped_by_initial" { value = local.grouped }
 output "Hotels1" {
   value = [for word in var.Hotels1 : (word == "Hyatt" ? upper(word) : word)]
 }
+outputs Theactivities_string = join(" → ", var.activities)
+}
