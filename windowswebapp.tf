12cab6f23356ba3021e6195f4393190b8023d87a
kamalkaur1 <gurs7401@gmail.com>
2025-09-24 20:08:16 -0400

    Update windowswebapp.tf

--- windowswebapp.tf
+++ windowswebapp.tf
@@ -1,23 +1,23 @@
 
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "kaur" {
+  name     = "mcitkaur"
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_plan" "kaurserviceplan" {
+  name                = "kaurplan"
+  resource_group_name = azurerm_resource_group.kaur.name
+  location            = azurerm_resource_group.kaur.location
   sku_name            = "P1v2"
   os_type             = "Windows"
 }
 
-resource "azurerm_windows_web_app" "example" {
+resource "azurerm_windows_web_app" "kaurwebapp" {
   name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+  resource_group_name = azurerm_resource_group.kaur.name
+  location            = azurerm_service_plan.kaur.location
+  service_plan_id     = azurerm_service_plan.kaur.id
 
   site_config {}
 }
