e174f4cd71eb5961523c332ac5dbcae7c16f0e12
kamalkaur1 <gurs7401@gmail.com>
2025-09-26 19:39:45 -0400

    Update vitualmachine.tf

--- vitualmachine.tf
+++ vitualmachine.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_virtual_machine" "VMlinux" {
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.VM.id,
+    azurerm_network_interface.VMlinux.id,
   ]
 
   admin_ssh_key {
