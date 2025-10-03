a0d2252a827dc68f40684961f51f2931d7861f2d
kamalkaur1 <gurs7401@gmail.com>
2025-10-03 18:37:20 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,26 +7,14 @@ terraform{
   }
   required_version=">=1.4.0"#this version is for Terraform Version, NOT azurerm
 }
-
+random = {
+     source  = "hashicorp/random"
+     version = ">= 3.6.0"
+   }
 provider "azurerm"{
   features{}  
   subscription_id=var.subscription_id
   client_id=var.client_id
   client_secret=var.client_secret
   tenant_id=var.tenant_id
-}
-
-terraform {
- required_version = ">= 1.5.0"
- required_providers {
-   azurerm = {
-     source  = "hashicorp/azurerm"
-     # Feel free to pin tighter, e.g. "~> 4.47"
-     version = ">= 4.40.0, < 5.0.0"
-   }
-   random = {
-     source  = "hashicorp/random"
-     version = ">= 3.6.0"
-   }
- }
 }
