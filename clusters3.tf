9b86b45ac5166d2b914e4fa54d5b242d646abc8e
kamalkaur1 <gurs7401@gmail.com>
2025-10-06 20:22:16 -0400

    Update clusters3.tf

--- clusters3.tf
+++ clusters3.tf
@@ -11,5 +11,4 @@ resource "azurerm_container_registry" "containers" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
     sku      = each.value.sku
-  node_count = each.value.node_count
-}
+ }
