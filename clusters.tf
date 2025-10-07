c9c068b4a7b7d0aaa68d22aaa5907a92e16e5875
kamalkaur1 <gurs7401@gmail.com>
2025-10-06 20:02:19 -0400

    Update clusters.tf

--- clusters.tf
+++ clusters.tf
@@ -28,23 +28,23 @@ resource "azurerm_kubernetes_cluster" "example" {
 }
 
 
-variable "3clusters" {
+variable "clusters3" {
   default = {
-   mcit-aks-1  = { sku = "B1", node_count = 1 }
-  mcit-aks-2 = { sku = "P1v3", node_count = 2 }
-"mcit-aks-1= { sku = "P1v3", node_count = 3 }
+   mcit-aks-1  ={ sku = "B1", node_count = 1 }
+  mcit-aks-2 ={ sku = "P1v3", node_count = 2 }
+"mcit-aks-1={ sku = "P1v3", node_count = 3 }
   }
 }
  
 resource "azurerm_registry" "asp2" {
-  for_each            = var.3clusters
+  for_each            = var.clusters3
   name                = "asp2-${each.key}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
     worker_count        = each.value.node_count
 }
-resource "azurerm_role_assignment" "3clusters" {
-  principal_id                     = azurerm_kubernetes_cluster.3clusters.kubelet_identity[0].object_id
+resource "azurerm_role_assignment" "clusters3" {
+  principal_id                     = azurerm_kubernetes_cluster.clusters3.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.3clusters.id
   skip_service_principal_aad_check = true
