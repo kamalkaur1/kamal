8b7ccc8cb7d0a30720fa2fa78218664265614fdc
kamalkaur1 <gurs7401@gmail.com>
2025-10-06 20:25:39 -0400

    Update clusters3.tf

--- clusters3.tf
+++ clusters3.tf
@@ -1,13 +1,13 @@
 variable "clusters3" {
   default = {
-  mcit-aks-1  = { sku = "B1", node_count = 1 }
-    mcit-aks-2 = { sku = "P1v3", node_count = 2 }
-mcit-aks-3 = { sku = "P1v3",node_count = 3 }
+  aks-1  = { sku = "B1", node_count = 1 }
+  aks-2 = { sku = "P1v3", node_count = 2 }
+aks-3 = { sku = "P1v3",node_count = 3 }
   }
 }
 resource "azurerm_container_registry" "containers" {
   for_each            = var.clusters3
-  name                = "${each.key}"
+  name                = "$mcit-{each.key}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
     sku      = each.value.sku
