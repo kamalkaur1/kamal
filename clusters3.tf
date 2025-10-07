1156a979c2dbf186bbd034200f105874d84e8f6c
kamalkaur1 <gurs7401@gmail.com>
2025-10-06 20:35:58 -0400

    Update clusters3.tf

--- clusters3.tf
+++ clusters3.tf
@@ -5,9 +5,16 @@ variable "clusters3" {
 mcit-aks-3 = { sku = "P1v3",node_count = 3 }
   }
 }
+locals {
+  clustersnames = [
+    "",
+    "mcit-aks-1",
+    "mcit-aks-2",
+    "mcit-aks-3",
+    ]
 resource "azurerm_container_registry" "containers" {
   for_each            = var.clusters3
-  name                = each.key
+  name                = locals.key
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
     sku      = each.value.sku
