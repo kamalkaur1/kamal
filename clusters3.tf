ecda93779904ee69c94047d381a593f3c30e28c3
kamalkaur1 <gurs7401@gmail.com>
2025-10-08 18:29:31 -0400

    Update clusters3.tf

--- clusters3.tf
+++ clusters3.tf
@@ -1,19 +1,2 @@
 
 
-variable "clusters3" {
-  default = {
-    mcit-aks-1  = { sku = "standard", nodecount = "1" }
-  mcit-aks-2 = { sku = "Premium", nodecount ="2" }
-mcit-aks-3 = { sku = "basic",nodecount = "3"  } 
-   
-}
-
-resource "clustersname" "this" {
-  for_each = var.clusters3
-
-  name     = each.key
-  version  = each.value.nodecount
- 
-  
-  }
-}
