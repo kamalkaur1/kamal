4a09d039873d8f0357bdd8e5330a5eb13e2238a8
kamalkaur1 <gurs7401@gmail.com>
2025-10-08 20:15:33 -0400

    Update clusters.tf

--- clusters.tf
+++ clusters.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "example" {
  resource_group_name = azurerm_resource_group.example.name
  location            = azurerm_resource_group.example.location
  sku                 = "Premium"
-}}
+}
 resource "azurerm_kubernetes_cluster" "example" {
  name                = "example-aks1"
  location            = azurerm_resource_group.example.location
