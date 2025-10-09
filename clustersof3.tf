7ab621554630ed1cfbb3431dfc140459d51cdb51
kamalkaur1 <gurs7401@gmail.com>
2025-10-08 20:05:57 -0400

    Update clustersof3.tf

--- clustersof3.tf
+++ clustersof3.tf
@@ -1,31 +1,3 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-
-
-
-
-
-
-variable "clusters3" {
-  default = {
-   cluster1  = { cidr_block = "10.0.0.0/16", node_count = 1 }
-    cluster1 = { cidr_block = "10.1.0.0/16", node_count = 2 }
-cluster1 = { cidr_block = "10.2.0.0/16", node_count = 3 }
-  }
-}
-
-
-resource "nameof3clusters" "CLUSTER" {
-  for_each            = var.clusters3
-  name                = "asp-${each.key}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = each.value.cidr_block
-  worker_count        = each.value.node_count
-}
 
 
 
