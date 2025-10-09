7cc8f2f9e802e7b108373f9dff102e899f29e058
kamalkaur1 <gurs7401@gmail.com>
2025-10-08 20:08:32 -0400

    Update clusters.tf

--- clusters.tf
+++ clusters.tf
@@ -1,72 +1,33 @@
 resource "azurerm_resource_group" "example" {
-provider "aws" {
-  region = "us-west-2"
+ name     = "example-resources"
+ location = "West Europe"
 }
-
-locals {
-  clusters = {
-    cluster1 = {
-      cidr_block = "10.0.0.0/16"
-    }
-    cluster2 = {
-      cidr_block = "10.1.0.0/16"
-    }
-    cluster3 = {
-      cidr_block = "10.2.0.0/16"
-    }
-  }
+resource "azurerm_container_registry" "example" {
+ name                = "containerRegistry1"
+ resource_group_name = azurerm_resource_group.example.name
+ location            = azurerm_resource_group.example.location
+ sku                 = "Premium"
 }
-
-# Create one VPC per cluster
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
-
-  for_each = local.clusters
-
-  name = "${each.key}-vpc"
-  cidr = each.value.cidr_block
-
-  azs             = ["us-west-2a", "us-west-2b"]
-  private_subnets = ["${cidrsubnet(each.value.cidr_block, 8, 1)}", "${cidrsubnet(each.value.cidr_block, 8, 2)}"]
-  public_subnets  = ["${cidrsubnet(each.value.cidr_block, 8, 3)}", "${cidrsubnet(each.value.cidr_block, 8, 4)}"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Environment = "dev"
-    Cluster     = each.key
-  }
+resource "azurerm_kubernetes_cluster" "example" {
+ name                = "example-aks1"
+ location            = azurerm_resource_group.example.location
+ resource_group_name = azurerm_resource_group.example.name
+ dns_prefix          = "exampleaks1"
+ default_node_pool {
+   name       = "default"
+   node_count = 1
+   vm_size    = "Standard_D2_v2"
+ }
+ identity {
+   type = "SystemAssigned"
+ }
+ tags = {
+   Environment = "Production"
+ }
 }
-
-# Create one EKS cluster per VPC
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
-
-  for_each = local.clusters
-
-  cluster_name    = each.key
-  cluster_version = "1.28"
-
-  subnet_ids = module.vpc[each.key].private_subnets
-  vpc_id     = module.vpc[each.key].vpc_id
-
-  enable_irsa = true
-
-  eks_managed_node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_types = ["t3.medium"]
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Cluster     = each.key
-  }
+resource "azurerm_role_assignment" "example" {
+ principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+ role_definition_name             = "AcrPull"
+ scope                            = azurerm_container_registry.example.id
+ skip_service_principal_aad_check = true
 }
