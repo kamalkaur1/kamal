45010d7fb886c2687b31f47b29c39816d98299b1
kamalkaur1 <gurs7401@gmail.com>
2025-10-08 20:14:03 -0400

    Update clusters3.tf

--- clusters3.tf
+++ clusters3.tf
@@ -1,2 +1,21 @@
+locals {
+  clusters = {
+    cluster1 = {
+      cidr_block     = "10.0.0.0/16"
+      instance_type  = "t3.medium"
+      cluster_version = "1.28"
+    }
+    cluster2 = {
+      cidr_block     = "10.1.0.0/16"
+      instance_type  = "t3.large"
+      cluster_version = "1.28"
+    }
+    cluster3 = {
+      cidr_block     = "10.2.0.0/16"
+      instance_type  = "t3.medium"
+      cluster_version = "1.27"
+    }
+  }
+}
 
 
