9b39caf77cc0f6b4208d37ebef1d4cb47ad2811e
kamalkaur1 <gurs7401@gmail.com>
2025-10-08 20:03:05 -0400

    Update clusters.tf

--- clusters.tf
+++ clusters.tf
@@ -1,30 +1,72 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+provider "aws" {
+  region = "us-west-2"
 }
-resource "azurerm_container_registry" "example" {
-  name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
+
+locals {
+  clusters = {
+    cluster1 = {
+      cidr_block = "10.0.0.0/16"
+    }
+    cluster2 = {
+      cidr_block = "10.1.0.0/16"
+    }
+    cluster3 = {
+      cidr_block = "10.2.0.0/16"
+    }
+  }
 }
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-  identity {
-    type = "SystemAssigned"
-  }
+
+# Create one VPC per cluster
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  for_each = local.clusters
+
+  name = "${each.key}-vpc"
+  cidr = each.value.cidr_block
+
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["${cidrsubnet(each.value.cidr_block, 8, 1)}", "${cidrsubnet(each.value.cidr_block, 8, 2)}"]
+  public_subnets  = ["${cidrsubnet(each.value.cidr_block, 8, 3)}", "${cidrsubnet(each.value.cidr_block, 8, 4)}"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
   tags = {
-    Environment = "Production"
+    Environment = "dev"
+    Cluster     = each.key
   }
-
 }
 
+# Create one EKS cluster per VPC
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
+  for_each = local.clusters
+
+  cluster_name    = each.key
+  cluster_version = "1.28"
+
+  subnet_ids = module.vpc[each.key].private_subnets
+  vpc_id     = module.vpc[each.key].vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Cluster     = each.key
+  }
+}
